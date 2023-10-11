8e8d15da118f8dcbb177cb9281d4dd3a1c88bd34
Yaroslav Yavornytskyi <ya.yaroslav27@gmail.com>
2023-10-11 08:45:37 +0200

    Correct array with unique teams

--- hw19/task_2.py
+++ hw19/task_2.py
@@ -3,7 +3,7 @@ df = pd.read_csv('https://raw.githubusercontent.com/guipsamora/pandas_exercises/
 
 selected_columns = df[['Team', 'Yellow Cards', 'Red Cards']]
 
-num_teams = df['Team'].unique()
+num_teams = df['Team'].nunique()
 print(f"{num_teams} teams participated in the Euro2012.")
 
 teams_more_than_6_goals = df[df['Goals'] > 6]
